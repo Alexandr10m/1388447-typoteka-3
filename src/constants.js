a16abe02c1f28e01253c423cb5ccbc28448f76bb
a.scherbatsky <a.scherbatsky@asferro.com>
2021-06-25 14:08:32 +0300

    Added command --server in Cli

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,21 @@ const ExitCode = {
   ERROR: 1,
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+};
+
+const Route = {
+  MAIN: `/`,
+}
+
 const USER_ARGV_INDEX = 2;
+
+const MOCKS_FILE_PATH = 'mocks.json';
 
 const DEFAULT_COMMAND = `--help`;
 
-module.exports = {ExitCode, USER_ARGV_INDEX, DEFAULT_COMMAND};
+const DEFAULT_PORT = 3000;
+
+module.exports = {ExitCode, USER_ARGV_INDEX, DEFAULT_COMMAND, HttpCode, Route, DEFAULT_PORT, MOCKS_FILE_PATH};
