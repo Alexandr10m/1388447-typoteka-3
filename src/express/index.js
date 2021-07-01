b8c300768c7a2e993fd9b5424497261d37151248
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-01 11:09:53 +0300

    Hooked up pug to express

--- src/express/index.js
+++ src/express/index.js
@@ -1,16 +1,24 @@
 "use strict";
 
 const express = require(`express`);
+const path = require(`path`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articleRoutes = require(`./routes/article-routes`);
 
 const DEFAULT_PORT = 8080;
+const TEMPLATES_DIR = `templates`;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articleRoutes);
+
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
