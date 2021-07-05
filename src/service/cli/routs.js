6462036dc087f9a5fcb6df3f7336f91bbbe54151
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-05 11:41:38 +0300

    Replaced the http module with express

---
+++ src/service/cli/routs.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const {Router} = require(`express`);
+const fs = require(`fs/promises`);
+const chalk = require(`chalk`);
+const {HttpCode, MOCKS_FILE_PATH} = require(`../../constants`);
+
+const postRouts = new Router();
+
+postRouts.get(`/`, async (req, res) => {
+  try {
+    const mocks = await fs.readFile(MOCKS_FILE_PATH);
+    const content = JSON.parse(mocks);
+
+    if (content.length === 0) {
+      res.send([]);
+    } else {
+      res.json(content);
+    }
+
+  } catch (err) {
+    console.error(chalk.red(err.message));
+    res.status(HttpCode.INTERNAL_SERVER_ERROR);
+    res.send([]);
+  }
+
+})
+module.exports = postRouts;
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,54 +1,21 @@
 'use strict';
 
-const http = require(`http`);
-const fs = require(`fs/promises`);
+const express = require(`express`);
 const chalk = require(`chalk`);
-const {HttpCode, Route, DEFAULT_PORT, MOCKS_FILE_PATH} = require(`../../constants`);
+const {DEFAULT_PORT} = require(`../../constants`);
+const postRouts = require(`./routs`);
 
-const sendMessage = (res, statusCode, content) => {
-  const template = `
-  <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>My first server on project - typoteka-3</title>
-      </head>
-      <body>${content}</body>
-    </html>`.trim();
-  res.writeHead(statusCode, {'Content-Type': `text/html; charset=UTF-8`});
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessage = `Not Found`;
-  switch (req.url) {
-    case Route.MAIN:
-      try {
-        const data = await fs.readFile(MOCKS_FILE_PATH, `utf8`);
-        const content = JSON.parse(data);
-        const titles = content.map((item) => `<li>${item.title}</li>`).join(``);
-
-        sendMessage(res, HttpCode.OK, `<ul>${titles}</ul>`);
-
-      } catch (err) {
-        console.error(`Error in response: `, err.message);
-        sendMessage(res, HttpCode.NOT_FOUND, notFoundMessage);
-      }
-      break;
-
-    default:
-      sendMessage(res, HttpCode.NOT_FOUND, notFoundMessage);
-      break;
-  }
-};
+const server = express();
+server.use(express.json());
+server.use(`/posts`, postRouts);
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = +customPort || DEFAULT_PORT;
-    const server = http.createServer(onClientConnect);
-    server.listen(port);
-    server.on(`listening`, () => console.log(chalk.green(`Waiting for connection on ${port}`)));
+
+    server.listen(port, () => console.info(chalk.green(`Waiting for connection on ${port}`)))
     server.on(`error`, ({message}) => console.error(`Server creation error: ${message}`));
   }
 };
