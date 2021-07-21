c203a8c5b874d6b6b7231ff0763f389578371d8b
a.scherbatsky <a.scherbatsky@asferro.com>
2021-07-21 10:34:14 +0300

    Added middlewares for logger

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,10 +1,28 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
 const {DEFAULT_PORT} = require(`../../constants`);
 const routerApi = require(`../api`);
 const {logger} = require(`../lib/logger`);
+const {HttpCode} = require(`../../constants`);
+
+const handlerRouteLog = (req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+};
+
+const handlerPageNotFound = (req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not Found`);
+  logger.error(`Route not found: ${req.url}`);
+};
+
+const handlerError = (err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+};
 
 module.exports = {
   name: `--server`,
@@ -13,6 +31,9 @@ module.exports = {
     const port = +customPort || DEFAULT_PORT;
     const server = express();
     server.use(express.json());
+    server.use(handlerRouteLog);
+    server.use(handlerError);
+    server.use(handlerPageNotFound);
 
     try {
       await routerApi(server);
